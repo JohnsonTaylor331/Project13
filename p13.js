d744ad3a94c6f7a44d98ef6ec8cc01925e064aa9
Taylor Johnson <johnsontaylor331@gmail.com>
2022-02-17 18:19:05 -0700

    first commit

---
+++ p13.js
@@ -0,0 +1,48 @@
+$('#box1').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box2').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box3').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box4').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box5').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box6').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box7').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box8').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box9').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box10').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box11').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box12').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box13').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box14').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box15').click( function() {
+    $(this).toggleClass('change');
+})
+$('#box16').click( function() {
+    $(this).toggleClass('change');
+})
